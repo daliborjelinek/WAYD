3ece773c356255d4b3a234e693b5fa49d37232d6
Dalibor Jelínek <daliborjelinek01@gmail.com>
2019-05-08 06:34:48 +0200

    Activity list UI Androidx migration

--- app/src/main/java/com/example/wayd/Activities/MainActivity.kt
+++ app/src/main/java/com/example/wayd/Activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.wayd.activities
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
-import android.util.Log
+import android.view.PointerIcon
+import androidx.appcompat.app.AppCompatActivity
+
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -11,19 +13,18 @@ import com.example.wayd.dbmanagersImpl.WAYDManagerImpl
 
 
 import com.example.wayd.R
-import com.example.wayd.dbentities.Activity
+import com.example.wayd.ui.RecyclerViewAdapter
 import com.example.wayd.dbmanagersImpl.ActivityManagerImpl
 import com.example.wayd.dbmanagersImpl.RecordManagerImpl
-import com.example.wayd.enums.Type
 import io.realm.Realm
-import java.lang.Exception
-import java.util.Date
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG: String = "MainActivity"
     }
-
+    private val activities: ArrayList<TmpActivity> = ArrayList()
     private val activityManager = ActivityManagerImpl()
     private val recordManager = RecordManagerImpl()
     private val WAYDManager = WAYDManagerImpl()
@@ -34,31 +35,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvActivityData = findViewById(R.id.tvActivityData) as TextView
-        button = findViewById(R.id.button) as Button
-        button.setOnClickListener{
-            var activity = Activity(activityManager.getNextPrimaryKey(realm), "Test", "RED", Double.MIN_VALUE, false, Type.perHour.toString())
-            activityManager.addOrUpdateActivity(realm, activity)
-            tvActivityData.text = activityManager.getallActivities(realm).toString()
-            tvActivityData.text = Date().toString()
-        }
-        try {
-            Realm.init(this.applicationContext)
-            realm = Realm.getDefaultInstance()
-            var activityEg = Activity(activityManager.getNextPrimaryKey(realm), "Test", "RED", Double.MIN_VALUE, false, Type.perHour.toString())
-            activityManager.addOrUpdateActivity(realm, activityEg)
-        } catch (e: Exception){
-            Log.e(TAG, "Exceptioin in crating realm object")
-        }
+
+       // setSupportActionBar(findViewById(R.id.action_bar))
+
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+
+        recyclerView.adapter = RecyclerViewAdapter(activities,this)
+
+        activities.add(TmpActivity("School","10:00","#f44336","500 $", "&#xf2bb;"))
+        activities.add(TmpActivity("Work","19:00","#2196f3","200 $", "&#xf0b1;"))
+        activities.add(TmpActivity("Sport","55:30","#009688","500 $", "&#xf45f;"))
+        activities.add(TmpActivity("Sport","55:30","#009688","500 $", "&#xf45f;"))
+        activities.add(TmpActivity("Work","19:00","#2196f3","200 $", "&#xf0b1;"))
+        activities.add(TmpActivity("Sport","55:30","#009688","500 $", "&#xf45f;"))
+        activities.add(TmpActivity("Sport","55:30","#009688","500 $", "&#xf45f;"))
+
+
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        realm.close()
-    }
+
 
     fun newActivity( v: View) {
         val intent = Intent(this, ActivityEditorActivity::class.java)
         startActivity(intent)
     }
 }
+
+data class TmpActivity(val name: String, val time: String, val color: String, val value: String,val icon: String)
